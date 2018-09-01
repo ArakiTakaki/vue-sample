4ff5cc806c2742b461f84c85b302a700ad4095a7
ArakiTakaki <araki_takaki@yahoo.co.jp>
2018-09-01 11:22:31 +0900

    コンパイルが通るようになった。

--- webpack.config.js
+++ webpack.config.js
@@ -10,33 +10,50 @@ module.exports = {
     path: `${__dirname}/build/`,
     filename: 'asset/js/bundle.js'
   },
+  // 開発サーバの設定
+  devServer: {
+    // destディレクトリの中身を表示してね、という設定
+    contentBase: `${__dirname}/build`,
+  },
   module: {
     rules: [
+      {
+        test: /\.vue$/,
+        use: [
+          { loader: 'vue-loader' }
+        ],
+      },
       {
         // load対象ファイル
         test: /\.js$/,
         exclude: /node_modules/,
         // loaderの設定
-        use: [{loader:'babel-loader'}]
+        use: [
+          {
+            loader: 'babel-loader',
+            // Babel のオプションを指定する
+            options: { presets: ['@babel/preset-env'] }
+          }
+        ]
       },
       {
         test: /\.html$/,
-        use:'html-loader'
+        use: 'html-loader'
       },
       {
         test: /\.css$/,
-        use:[
-          {loader:'style-loader'},
-          {loader:'css-loader'},
-          {loader:'sass-loader'}
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' }
         ]
       }
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({template: `${__dirname}/public/index.html`})
+    new HtmlWebpackPlugin({ template: `${__dirname}/public/index.html` })
   ],
   resolve: {
-    extensions: ['.js', '.vue'],
+    extensions: ['.js', '.vue', '.html', '.sass', '.scss'],
   }
 }
